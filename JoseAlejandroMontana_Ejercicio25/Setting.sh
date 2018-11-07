7b844cffbb7b2d3d8f86a6f22c9b3a0b2fc2fcaf
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-07 07:51:24 -0500

    sh para ejecutar todo en el cluster

---
+++ JoseAlejandroMontana_Ejercicio25/Setting.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#PBS -l nodes=1:intel:ppn=8, mem=64gb
+#PBS -l walltime=7:00:00:00
+#PBS -M ja.montana@uniandes.edu.co
+#PBS -m abe
+#PBS -N test1 gromacs
+#PBS -j oe
+#PBS -o joboutput.txt
+
+module load anaconda/python2
+make -f Makefilev1
