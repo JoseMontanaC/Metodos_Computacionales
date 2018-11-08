307b58f4f4398c99d57ea378a4eb30451d0d5c17
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-08 18:07:08 -0500

    Archivos de gráficas con sus respectivas figuras

---
+++ JoseAlejandroMontana_Ejercicio26/plot_times.py
@@ -0,0 +1,33 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+archivo= np.genfromtxt("Tiempos.txt")
+div=archivo[:,0]
+times = archivo[:,1]
+D={}
+for i in range(len(div)):
+    if div[i] in D:
+        D[div[i]].append(times[i])
+    else:
+        D[div[i]] = [times[i]]
+X=[]
+Y=[]
+Y_err=[]
+for i in sorted(D.keys()):
+    Y.append(np.mean(D[i]))
+    X.append(i)
+    Y_err.append(np.std(D[i]))
+ 
+X=np.array(X)
+Y=np.array(Y)
+Y_err=np.array(Y_err)
+# los datos estan el microsegundo, entonces lo pasamos a mili multiplicando por 10^-3
+plt.figure(figsize=(10,6))
+plt.errorbar(X,Y,Y_err,color="firebrick")
+plt.scatter(X,Y,color="navy")
+# plt.plot(X,Y)
+plt.title("Tiempo del algoritmo en funcion de la particion")
+plt.xlabel("Particion")
+plt.ylabel("Tiempo de computo")
+plt.savefig("Grafica_tiempo.pdf")
+plt.show()
