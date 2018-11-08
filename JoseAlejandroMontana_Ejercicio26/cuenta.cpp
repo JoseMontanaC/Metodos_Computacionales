547029a2c7d2185f0ade855903e6a73904011977
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-08 12:29:01 -0500

    Programa que escribe en los archivos y ademas retorna el tiempo al dividir

--- JoseAlejandroMontana_Ejercicio26/cuenta.cpp
+++ JoseAlejandroMontana_Ejercicio26/cuenta.cpp
@@ -3,6 +3,7 @@
 #include<string>
 // #include <sstream>
 #include <chrono>
+#include<vector>
 int main(int argc, char **argv)
 {
   std::ifstream inFile;
@@ -107,10 +108,68 @@ int main(int argc, char **argv)
   inFile.close();
   cuenta.close();
   tiempo.close();
-  // Dividir el string
-  for(int i =0; i<line.size();i++)
+
+  std::vector<int> d_list = {10,20,50,100};
+  for (auto division : d_list)
     {
-      std::cout<<line[i]<<std::endl;
+      // Dividir el string
+      int r=line.size()/division;
+      double tiempo=0.0;
+      for(int kk=0; kk<division;kk++)
+	{
+	  std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+	  std::string intento;
+	  int C_1=0.0;
+	  int C_2=0.0;
+	  int C_3=0.0;
+	  int C_4=0.0;
+	  int C_5=0.0;
+	  for(int ii = kk*r;ii<(kk+1)*r;ii++)
+	    {
+	  
+	      if(ii+patron_1.size()>(kk+1)*r)
+		{
+		  break;
+		}
+	      for(int jj=ii;jj<=ii+patron_5.size();jj++)
+		{
+		  intento +=line[jj];
+		  
+		  if (intento==patron_1)
+		    {
+		      C_1 += 1;
+		    }
+		  if (intento==patron_2)
+		    {
+		      C_2 += 1;
+		    }
+		  if (intento==patron_3)
+		    {
+		      C_3 += 1;
+		    }
+		  if (intento==patron_4)
+		    {
+		      C_4 += 1;
+		    }
+		  if (intento==patron_5)
+		    {
+		      C_5 += 1;
+		    }
+		  // std::cout<<intento<<std::endl;
+		  
+		}
+	      intento.clear();
+	      // std::cout<<std::endl;
+	    }         
+	  std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+	  double duration = std::chrono::duration_cast<std::chrono::microseconds>( t2 - t1 ).count();
+	  std::cout<<division<<"\t"<<duration<<std::endl;
+	  // tiempo += duration<std::chrono::nanoseconds>.count();
+	  tiempo += std::chrono::duration_cast<std::chrono::nanoseconds>( t2 - t1 ).count();
+	}
+      // std::cout<<dur<<std::endl;
+      
     }
+      
   return 0;
 }
