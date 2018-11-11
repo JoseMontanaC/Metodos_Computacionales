df938edb42c637de25bdfe15f85c24d6aa74467f
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-11 15:14:24 -0500

    Programa en bash que envia el tiempo sobre cada partición a un txt

--- JoseAlejandroMontana_Ejercicio26/Send_process.sh
+++ JoseAlejandroMontana_Ejercicio26/Send_process.sh
@@ -1,3 +1,4 @@
+g++ -std=c++11 cuenta2.cpp -o Main.x
 carpetas=Part_*
 for i in $carpetas
 do
@@ -6,8 +7,9 @@ do
     echo "Empieza el analisis del tiempo de la particion" $i
     for j in $archivos
     do
-	g++ ../cuenta2.cpp
-	./a.out $j > tiempos.txt
+    	../Main.x $j > tiempos.txt
     done
+    # rm a.out
     cd ..
 done
+rm Main.x
