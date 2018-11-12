1bf18a9702ce524c624c9e958f9287d183033710
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-11 21:01:26 -0500

    Carpetas con los cortes el programa en bash que ejecuta el codigo Cuenta_to_file.cpp y corre esto para cada archivo, luego analiza todos y manda el output a un archivo Tiempos.txt

---
+++ JoseAlejandroMontana_Ejercicio26/Part_10/Run_code.sh
@@ -0,0 +1,28 @@
+archivos=corte_*
+g++ -std=c++11 ../Cuenta_to_file.cpp -o Main.x
+len=$(ls corte_* | wc -l)
+longitud=$(seq 1 1 $len)
+if [ -f Tiempos ]
+then
+    rm -r Tiempos
+else
+    mkdir Tiempos
+    cd Tiempos
+fi
+
+for ((kk=1;kk<=$len;kk++))
+do
+    arch="../corte_$kk.txt"
+    ../Main.x $arch "tiempo_$kk.txt" &
+done
+tiempo=tiempo_*
+cat $tiempo > Tiempos.txt
+cd ..
+rm Main.x
+    # for ((kk=1;kk<=$len;kk++))
+    # do   
+    # done
+    # mkdir Tiempos
+    # cd Tiempos
+    # ../Main.x $j  tiempos
+
