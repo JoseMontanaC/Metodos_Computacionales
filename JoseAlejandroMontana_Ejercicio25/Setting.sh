cedaee8f838050a8bb545de4a24674d3dbd5032c
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-13 17:58:20 -0500

    nuevo Setting para mandar tareas al cluster

--- JoseAlejandroMontana_Ejercicio25/Setting.sh
+++ JoseAlejandroMontana_Ejercicio25/Setting.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#PBS -l nodes=1:intel:ppn=8, mem=64gb
-#PBS -l walltime=7:00:00:00
+#PBS -l nodes=1:ppn=8,mem=1gb,walltime=00:10:00
 #PBS -M ja.montana@uniandes.edu.co
 #PBS -m abe
 #PBS -N Ejercicio25
-#PBS -j oe
-#PBS -o joboutput.txt
 
 module load anaconda/python2
+cd $PBS_O_WORKDIR
 make -f Makefilev1 clean
 make -f Makefilev1
