0b8040e4e52d97cab4aa039c53ee873cc302fff6
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-14 08:51:57 -0500

    a

--- JoseAlejandroMontana_Ejercicio27/Gauss.c
+++ JoseAlejandroMontana_Ejercicio27/Gauss.c
@@ -12,24 +12,26 @@ int main (int argc, char *argv[])
   float mu, sigma;
   long long i;
   int rank, size;
-   MPI_Init (&argc, &argv);
-   MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-   MPI_Comm_size (MPI_COMM_WORLD, &size);
-   n_part= atoi(argv[1]);
+  FILE *out;
+  char name[100];
+  MPI_Init (&argc, &argv);
+  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+  MPI_Comm_size (MPI_COMM_WORLD, &size);
+  n_part= atoi(argv[1]);
   n_points = atoi(argv[2]);
   mu = atof(argv[3]);
   sigma = atof(argv[4]);
+
+  if(!(list=malloc(n_points * sizeof(double))))
+    {
+      fprintf(stderr, "Problema con la reserva de memoria\n");
+      exit(1);
+    }
   
-
-
-  if(!(list=malloc(n_points * sizeof(double)))){
-    fprintf(stderr, "Problema con la reserva de memoria\n");
-    exit(1);
-  }
-
   
   srand48(n_points);
-  for(i=0;i<n_points;i++){    
+  for(i=0;i<n_points;i++)
+    {    
       x = drand48();
       y = drand48();
       gauss_1 = sqrt(-2.0 * log(x)) * cos(2.0* PI * y);
@@ -37,31 +39,28 @@ int main (int argc, char *argv[])
       
       gauss_1 = gauss_1*sigma + mu;
       list[i] = gauss_1;
-  }
-
-
+    }
   
-  for(int jj=1;jj<=n_part;jj++)
-  {
-    FILE *out;
-    char name=sprintf(result,"sample_%d.dat", jj);
-  if(!(out = fopen(name, "w"))){
+  
+  sprintf(name,"sample_%d.dat", jj);
+  
+  if(!(out = fopen(name, "w")))
+    {
     fprintf(stderr, "Problema abriendo el archivo\n");
     exit(1);
-  }
-   /* for(i=0;i<n_points;i++) */
-   /*   { */
-   /*     printf("numero aleaotrio de %d of %d %f\n", rank, size,list[i]); */
-   /*   } */
-   for(i=0;i<n_points;i++){
-     fprintf(out, "%f\n", list[i]);
-   }
-    fclose(out);
-  }
+    }
+  /* for(i=0;i<n_points;i++) */
+  /*   { */
+  /*     printf("numero aleaotrio de %d of %d %f\n", rank, size,list[i]); */
+  /*   } */
+  for(i=0;i<n_points;i++)
+    {
+      fprintf(out, "%f\n", list[i]);
+    }
+  fclose(out);
+  MPI_Barrier(MPI_COMM_WORLD);
   MPI_Finalize();
- 
-  
-
+    
   return 0;
  
 }
