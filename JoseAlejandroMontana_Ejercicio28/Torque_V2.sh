65ac203ca76dfa60df390d28547ceff7836d9b7d
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-16 12:04:34 -0500

    todo

--- JoseAlejandroMontana_Ejercicio28/Torque_V2.sh
+++ JoseAlejandroMontana_Ejercicio28/Torque_V2.sh
@@ -2,7 +2,7 @@
 #PBS -l nodes=1:ppn=8,mem=1gb,walltime=00:10:00
 #PBS -M ja.montana@uniandes.edu.co
 #PBS -m abe
-#PBS -N Ejercicio25
+#PBS -N Ejercicio28
 
 module load anaconda/python2
 cd $PBS_O_WORKDIR
