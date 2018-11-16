9e5ec675fe783a3da3e40f6b4c2de142f72a3952
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-16 06:43:16 -0500

    Integral para varias variables

---
+++ JoseAlejandroMontana_Ejercicio28/Integrate.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include<cmath>
+#include<vector>
+#include<random>
+#include "mpi.h"
+
+double function_to_integrate(double *x,int size);
+const double lowerLimit = 0.0;
+const double upperLimit = 1.0;
+
+int main(int argc, char *argv[])
+{
+  std::mt19937 generator(1);
+  int dest, size, rank, tag;
+  int N = atoi(argv[1]);
+  double Lambda = 1.0;
+  MPI_Status status;
+  double area, punto, heigth, width, total, range, lower;
+  double lista[10];
+  
+  // Inicializar
+  for (int ii = 0; ii < N_prime; ii++)
+    {
+      lista[ii]=0.0; 
+    }
+  
+  // /* MPI setup */
+  // MPI_Init(&argc, &argv);
+  // MPI_Comm_size(MPI_COMM_WORLD, &size);
+  // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  // /* Adjust problem size for sub-process */
+  // range = (upperLimit - lowerLimit) / size;
+  // width = range / N;
+  // lower = lowerLimit + range*rank;
+
+  // area=0.0;
+  // // std::exponential_distribution<double> expon(Lambda);
+  // std::uniform_real_distribution<double> number(lowerLimit,upperLimit);
+  // for (int ii = 0; ii < N; ++ii)
+  //   {
+  //     punto = expon(generator);
+  //     heigth = Part_function(punto);
+  //     area += heigth/(double) (N*size);
+  //     // punto = lower + ii*width + width/2.0;
+  //     // heigth = Part_function(punto);
+  //     // area = area + width*heigth;
+  //   }
+
+  // /* Collect info and print results */
+  // tag = 0;
+  // if (0 == rank)
+  //   { /* Master */
+  //     total = area; 
+  //     for (int src = 1; src < size; ++src)
+  // 	{
+  // 	  MPI_Recv(&area, 1, MPI_DOUBLE, src, tag, MPI_COMM_WORLD, &status);
+  // 	  total += area;
+  // 	}
+  //     fprintf(stderr, "The area from %g to infinity is : %25.16e\n", lowerLimit, total);
+  //   }
+  // else
+  //   { /* slaves only send */
+  //     dest = 0; 
+  //     MPI_Send(&area, 1, MPI_DOUBLE, dest, tag, MPI_COMM_WORLD);
+  //   } 
+  
+  // /* finish */
+  // MPI_Finalize();
+
+  
+  return 0;
+
+
+
+  
+}
+
+
+double function_to_integrate(double *x,int size)
+{
+  double cuadratic=0.0;
+  for(int ii=0; ii<size;ii++)
+    {
+      cuadratic += x[ii];
+    }
+  cuadratic = cuadratic*cuadratic;
+  return cuadratic;
+}
