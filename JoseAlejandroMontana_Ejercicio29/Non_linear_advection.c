5cfbe8972ba9372f4f289f78eb8b474e95340f6b
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-20 22:57:18 -0500

    implementacion de Lax

--- JoseAlejandroMontana_Ejercicio29/Non_linear_advection.c
+++ JoseAlejandroMontana_Ejercicio29/Non_linear_advection.c
@@ -4,7 +4,7 @@
 
 void Flux(double *flux,double *u,int n);
 void Start_u(double *u,double *x, int n);
-/* void Lax(double *u,double t_max, double delta_t, double delta_x); */
+void Lax(double *u_final, double *u, double *F, double t_max, double delta_t, double delta_x,int n);
 int main(int argc, char ** argv)
 {
   /* Archivo para escribir */
@@ -25,35 +25,17 @@ int main(int argc, char ** argv)
   for( int ii=0; ii < n;ii++)
     {
       x[ii] = ii*0.05;
-      /* printf("%.5f \n",x[ii]); */
     }
   
-  
-  
-  /* for( int ii=0; ii < n;ii++) */
-  /*   { */
-  /*     if(x[ii]<2.0) */
-  /* 	{ */
-  /* 	  u[ii] = 1.0; */
-  /* 	} */
-  /*     else */
-  /* 	{ */
-  /* 	  u[ii] = 0.0; */
-  /* 	} */
-  /*   } */
-
     Start_u(u,x,n);
   
   /* Se escribe el archivo con las condiciones iniciales */
-
-
   
   if(!(out = fopen("Inicial.txt", "w")))
     {
       fprintf(stderr, "Problema abriendo el archivo\n");
       exit(1);
     }
-  
   for(int i=0;i<n;i++)
     {
       fprintf(out, "%.5f  %.5f\n", x[i],u[i]);
@@ -61,6 +43,7 @@ int main(int argc, char ** argv)
   
   
   /* Se definen la variables a utilizar para el metodo */
+
   double t_max=0.5, delta_t=0.5*delta_x;
   int N_t = t_max/delta_t;
   
@@ -75,7 +58,7 @@ int main(int argc, char ** argv)
       for(int jj = 1; jj< n-1;jj++)
 	{
 	  u_final[jj] = 0.5*(u[jj+1] +u[jj-1])
-	    - 0.5*(delta_t/delta_x)*(F[jj+1]-F[jj-1]);;
+	    - 0.5*(delta_t/delta_x)*(F[jj+1]-F[jj-1]);
 	  /* u_final[jj] -= 0.5*(delta_t/delta_x)*(F[jj+1]-F[jj-1]); */
 	  u[jj] = u_final[jj];
 	  /* printf("%.5f terminacon %.5f \n",u[jj],u_final[jj]); */
@@ -128,13 +111,21 @@ void Start_u(double *u,double *x, int n)
 	}
     }
 }
-/* void Lax(double *u,double t_max, double delta_t, double delta_x) */
-/* { */
-/*   int N_t = t_max/t_delta_t; */
-/*   double *flux = (double *) malloc(sizeof(double)*n); */
-/*   for(int jj=0; jj< N_t; jj++) */
-/*     { */
-      
-/*     } */
-/*   free(flux) */
-/* } */
+
+
+void Lax(double *u_final, double *u, double *F, double t_max, double delta_t, double delta_x,int n)
+{
+  int N_t = t_max/delta_t;
+  u_final[n-1]=u[n-1];
+  u_final[0]=u[0];
+  for(int ii= 0; ii<N_t ;ii++)
+    {
+      Flux(F,u,n);
+      for(int jj = 1; jj< n-1;jj++)
+	{
+	  u_final[jj] = 0.5*(u[jj+1] +u[jj-1])
+	    - 0.5*(delta_t/delta_x)*(F[jj+1]-F[jj-1]);
+	  u[jj] = u_final[jj];
+	}
+    }
+}
