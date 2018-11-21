e1937d43951b20f246eaa98098052e67148eebbf
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-20 22:50:31 -0500

    funcion para inicializar

--- JoseAlejandroMontana_Ejercicio29/Non_linear_advection.c
+++ JoseAlejandroMontana_Ejercicio29/Non_linear_advection.c
@@ -3,7 +3,7 @@
 #include <math.h>
 
 void Flux(double *flux,double *u,int n);
-
+void Start_u(double *u,double *x, int n);
 /* void Lax(double *u,double t_max, double delta_t, double delta_x); */
 int main(int argc, char ** argv)
 {
@@ -13,45 +13,58 @@ int main(int argc, char ** argv)
   int L = 4;
   double delta_x = 0.05;
   int n = L/delta_x +1;
+  char filename[128];
+
+   /* Arreglos para guardar el flujo, la configuracion final, la configuracion inicial y el espaciamiento */
+  
   double *x = (double *) malloc(sizeof(double)*n);
+  double *u = (double *) malloc(sizeof(double)*n);
+  double *F = (double *) malloc(sizeof(double)*n);
+  double *u_final = (double *) malloc(sizeof(double)*n);
+  
   for( int ii=0; ii < n;ii++)
     {
       x[ii] = ii*0.05;
       /* printf("%.5f \n",x[ii]); */
     }
   
-  double *u = (double *) malloc(sizeof(double)*n);
-  /* double *aux = (double *) malloc(sizeof(double)*n); */
-  for( int ii=0; ii < n;ii++)
-    {
-      if(x[ii]<2.0)
-	{
-	  u[ii] = 1.0;
-	}
-      else
-	{
-	  u[ii] = 0.0;
-	}
-    }
+  
+  
+  /* for( int ii=0; ii < n;ii++) */
+  /*   { */
+  /*     if(x[ii]<2.0) */
+  /* 	{ */
+  /* 	  u[ii] = 1.0; */
+  /* 	} */
+  /*     else */
+  /* 	{ */
+  /* 	  u[ii] = 0.0; */
+  /* 	} */
+  /*   } */
+
+    Start_u(u,x,n);
   
   /* Se escribe el archivo con las condiciones iniciales */
-  
-  if(!(out = fopen("Inicial.txt", "w"))){
-    fprintf(stderr, "Problema abriendo el archivo\n");
-    exit(1);
-  }
-  for(int i=0;i<n;i++){
-    fprintf(out, "%.5f  %.5f\n", x[i],u[i]);
-  }
 
+
+  
+  if(!(out = fopen("Inicial.txt", "w")))
+    {
+      fprintf(stderr, "Problema abriendo el archivo\n");
+      exit(1);
+    }
+  
+  for(int i=0;i<n;i++)
+    {
+      fprintf(out, "%.5f  %.5f\n", x[i],u[i]);
+    }
+  
   
   /* Se definen la variables a utilizar para el metodo */
-  double t_max=0.45, delta_t=0.015;
+  double t_max=0.5, delta_t=0.5*delta_x;
   int N_t = t_max/delta_t;
-
-  /* Arreglos para guardar el flujo y la configuracion final */
-  double *F = (double *) malloc(sizeof(double)*n);
-  double *u_final = (double *) malloc(sizeof(double)*n);
+  
+ 
   
   /* Se ponen las mismas condiciones de contorno */
   u_final[n-1]=u[n-1];
@@ -69,11 +82,12 @@ int main(int argc, char ** argv)
 	}
     }
   /* Se escribe el estado final */
-
-  if(!(out = fopen("Final.txt", "w"))){
-    fprintf(stderr, "Problema abriendo el archivo\n");
-    exit(1);
-  }
+  
+  if(!(out = fopen("Final.txt", "w")))
+    {
+      fprintf(stderr, "Problema abriendo el archivo\n");
+      exit(1);
+    }
   /* for(int i=0;i<n;i++){ */
   /*   fprintf(out, "%.5f  %.5f\n", x[i],u[i]); */
   /* } */
@@ -100,6 +114,20 @@ void Flux(double *flux,double *u,int n)
     }
 }
 
+void Start_u(double *u,double *x, int n)
+{
+  for(int ii = 0; ii<n;ii++)
+    {
+       if(x[ii]<2.0)
+	{
+	  u[ii] = 1.0;
+	}
+      else
+	{
+	  u[ii] = 0.0;
+	}
+    }
+}
 /* void Lax(double *u,double t_max, double delta_t, double delta_x) */
 /* { */
 /*   int N_t = t_max/t_delta_t; */
