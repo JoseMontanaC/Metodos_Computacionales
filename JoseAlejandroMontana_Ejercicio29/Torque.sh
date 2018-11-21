51d273a188caa7ff224d368af5ba78be62696ec4
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-21 06:57:11 -0500

    Torque

--- JoseAlejandroMontana_Ejercicio29/Torque.sh
+++ JoseAlejandroMontana_Ejercicio29/Torque.sh
@@ -6,9 +6,9 @@
 
 # module load anaconda/python2
 # cd $PBS_O_WORKDIR
-# gcc-8 -fopenmp Helloworld.c -o Hello.x
-# ./Hello.x
-# rm *.x
+gcc-8 -fopenmp Helloworld.c -o Hello.x
+./Hello.x
+rm *.x
 rm *.txt
 gcc Non_linear_advection.c -o Non_linear.x
 ./Non_linear.x
