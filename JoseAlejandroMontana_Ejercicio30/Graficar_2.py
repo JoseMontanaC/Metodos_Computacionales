a182cc78722ff306a295ef0de61a8ef1c5afef8f
Jose Alejandro Montana <ja.montana@uniandes.edu.co>
2018-11-23 07:38:39 -0500

    archivo que grafica el walk

--- JoseAlejandroMontana_Ejercicio30/Graficar_2.py
+++ JoseAlejandroMontana_Ejercicio30/Graficar_2.py
@@ -1,3 +1,5 @@
+import matplotlib
+matplotlib.use("Agg")
 import numpy as np
 import matplotlib.pylab as plt
 
@@ -5,4 +7,5 @@ archivo=np.genfromtxt("Walk_serial.txt")
 plt.hist(archivo,density=True,bins=100,label="Serial")
 archivo=np.genfromtxt("Walk_parallel.txt")
 plt.hist(archivo,density=True,bins=100,alpha=0.5,label="Parallel")
-plt.show()
+plt.savefig("walk_comparacion.pdf")
+plt.close()
